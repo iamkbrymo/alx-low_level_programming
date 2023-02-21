f70cc03ec1487ce449a625907eb8d494c1b27848
Michael Buraimoh <buraimohkolapo@gmail.com>
2023-02-21 09:10:47 -0800

    functions_nested_loops

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+ * time_table - prints a 9 times table
+ */
+void time_table(void)
+{
+	int num = 0, multi, prod;
+
+	while (num <= 9)
+	{
+		_putchar('0');
+		multi = 1;
+
+		while (multi <= 9)
+		{
+			_putchar(',');
+			_putchar(' ');
+
+			prod = num * multi;
+
+			if (prod <= 9)
+				_putchar(' ');
+			else
+				_putchar((prod / 10) + '0');
+
+			_putchar((prod % 10) + '0');
+
+			multi++;
+		}
+		_putchar('\n');
+		num++;
+	}
+}
