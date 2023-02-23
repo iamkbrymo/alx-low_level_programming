6df9a39bbaf7324a24f1033db24ffef51d51a5c5
Michael Buraimoh <buraimohkolapo@gmail.com>
2023-02-23 08:56:28 -0800

    more_functions_nested_loops

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * print_numbers - prints numbers from 0 to 9
+ */
+void print_numbers(void)
+{
+	char z = '0';
+
+	while (z < '10')
+	{
+		_putchar(z);
+	}
+	_putchar('\n');
+}
