a1d93e48abe6afbf3df50be56a18504d3dc50b3d
Michael Buraimoh <buraimohkolapo@gmail.com>
2023-03-02 08:15:38 -0800

    C - More pointers, arrays and strings

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -7,44 +7,26 @@
  */
 char *leet(char *s)
 {
-	int i = 0;
+	int a, b;
 
-	while (s[i] != '\0')
+	char *c = "aAeEoOtTlL";
+	char *d = "4433007711";
+
+	a = 0;
+
+	while (s[a] != '\0')
 	{
-		if (s[i] >= 'a' && s[i] <= 'z')
-		{
-			if (s[i] == 'a')
-				s[i] = s[i] - 45;
-			else if (s[i] == 'e')
-				s[i] = s[i] - 50;
-			else if (s[i] == 'o')
-				s[i] = s[i] - 63;
-			else if (s[i] == 't')
-				s[i] = s[i] - 61;
-			else if (s[i] == 'l')
-				s[i] = s[i] - 59;
-		}
-		i++;
-	}
+		b = 0;
 
-	i = 0;
-
-	while (s[i] != '\0')
-	{
-		if (s[i] >= 'A' && s[i] <= 'Z')
+		while (b < 10)
 		{
-			if (s[i] == 'A')
-				s[i] = s[i] - 13;
-			else if (s[i] == 'E')
-				s[i] = s[i] - 18;
-			else if (s[i] == 'O')
-				s[i] = s[i] - 31;
-			else if (s[i] == 'T')
-				s[i] = s[i] - 29;
-			else if (s[i] == 'L')
-				s[i] = s[i] - 27;
+			if (s[a] == c[b])
+			{
+				s[a] = d[b];
+			}
+			b++;
 		}
-		i++;
+		a++;
 	}
-	return (s);
+	return (S);
 }
