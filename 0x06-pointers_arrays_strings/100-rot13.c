489a50e3aff7b4ee50bccf7ac23c96c05f3d5545
Michael Buraimoh <buraimohkolapo@gmail.com>
2023-03-02 09:40:59 -0800

    C - More pointers, arrays and strings

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * rot13 - encodes a string into 1337
+ * @s: string to modify
+ * Return: returns encoded string.
+ */
+char *rot13(char *)
+{
+	int a, b;
+
+	char *c = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	char *d = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+
+	a = 0;
+
+	while (s[a] != '\0')
+	{
+		b = 0;
+
+		while (b < 52)
+		{
+			if (s[a] == c[b])
+			{
+				s[a] = d[b];
+			}
+			b++;
+		}
+		a++;
+	}
+	return (s);
+}
