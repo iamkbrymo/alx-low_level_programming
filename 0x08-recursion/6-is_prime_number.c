b6ab92aeed075d72a7ee763d2b0451dc8a8dc1fd
Michael Buraimoh <buraimohkolapo@gmail.com>
2023-03-08 13:30:11 -0800

    is_prime_number

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -29,6 +29,7 @@ int is_prime_number(int n)
 		return (0);
 	else if (n == 2)
 		return (1);
-	else if ((check_is_prime(n, 2) != 0)
+	return (check_is_prime(n, 2));
+	if ((check_is_prime(n, 2)) != 0)
 		return (1);
 }
