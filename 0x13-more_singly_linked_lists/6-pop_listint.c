563e12a94777fc760efb4ea1be48def86b55436a
Michael Buraimoh <buraimohkolapo@gmail.com>
2023-04-04 04:55:42 -0700

    Update to task 6, 7 and 9

--- 0x13-more_singly_linked_lists/6-pop_listint.c
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -12,11 +12,11 @@ int pop_listint(listint_t **head)
 	listint_t *new;
 	int n;
 
-	if (!*head)
+	if (!*head || !head)
 		return (0);
 
-	new = *head->next;
-	n = *head->n;
+	new = (*head)->next;
+	n = (*head)->n;
 	free(*head);
 	*head = new;
 
--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -13,12 +13,12 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 	listint_t *tmp;
 	unsigned int i = 0;
 
-	if (!head)
+	if (!head || !*head)
 		return (NULL);
 
 	tmp = head;
 
-	while (tmp->next && i <= index)
+	while (tmp && i < index)
 	{
 		tmp = tmp->next;
 		i++;
