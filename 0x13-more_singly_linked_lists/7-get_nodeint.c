4150ec84220c8b38ada7f5f6ba54094bff3b009d
Michael Buraimoh <buraimohkolapo@gmail.com>
2023-04-04 11:01:51 -0700

    Quick fix of task 7 and 9

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -10,7 +10,7 @@
 
 listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 {
-	listint_t *tmp = head;
+	listint_t *tmp = *head;
 	unsigned int i = 0;
 
 	if (!head || !*head)
--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -22,29 +22,26 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		return (NULL);
 
 	new->n = n;
-	new->next = NULL;
 
 	if (idx == 0)
 	{
-		new->next = *head;
+		new->next = head;
 		*head = new;
 		return (new);
 	}
 
 	i = 0;
-	while (tmp && i < idx)
+	while (i < (idx - 1))
 	{
-		if (i == (idx - 1))
-		{
-			new->next = tmp->next;
-			tmp->next = new;
-			return (new);
-		}
-		else
-			tmp = tmp->next;
+		if (tmp == NULL || tmp->next == NULL)
+			return (NULL);
 
+		tmp = tmp->next;
 		i++;
 	}
 
-	return (NULL);
+	new->next = tmp->next;
+	tmp->next = new;
+
+	return (new);
 }
